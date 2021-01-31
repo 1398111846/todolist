737c36ad54cacc5f5de1ed50aec415467b49862a
lishuqi <971576035@qq.com>
2021-01-31 15:56:39 +0800

    feat：add AppBar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <transition>
+      <router-view/>
+    </transition>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style>
+@import url('https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.css');
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-bar title="TODO" left="bars" right="search" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import AppBar from '../components/AppBar.vue'
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    AppBar,
   }
 }
 </script>
+
+<style>
+  .home {
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    color: black;
+  }
+</style>
